014b79911e679b65951b51c5faf7740d6e7dc484
yizhizoe <gao_yizhi@126.com>
2018-10-12 17:00:40 +0800

    test

--- tests/test_blue_green.py
+++ tests/test_blue_green.py
@@ -3,6 +3,6 @@ import os
 
 def test_green_service(port='8080'):
     alb_url = os.getenv('ALB_URL')
-    print (alb_url)
     response = requests.get(':'.join([alb_url, port]))
-    assert 'V4' in response
+    assert response.status_code == 200
+    assert 'V4' in response.text
